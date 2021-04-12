472b2534c5710ac9e313f856f60a63aaab8cb7e3
Guilherme Pessoa Correia <guilhermepc@brq.com>
2021-04-12 10:47:19 -0300

    CRUD dba

--- microservico/src/main/java/br/com/brq/controllers/TipoClienteController.java
+++ microservico/src/main/java/br/com/brq/controllers/TipoClienteController.java
@@ -3,22 +3,53 @@ package br.com.brq.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.brq.models.TipoCliente;
 import br.com.brq.services.TipoClienteService;
 
+
+@RequestMapping("tipo-clientes")//pega sempre com esse inicio de path
 @RestController
 public class TipoClienteController {
 	
 	@Autowired
 	private TipoClienteService tipoClienteService;
 
-	@GetMapping("tipo-clientes")
+	@GetMapping("")
 	public List<TipoCliente> findAll(){
 		return this.tipoClienteService.findAll();
 		
 		
 	}
+	
+	@GetMapping ("/{idtipocliente}")
+	public TipoCliente findOne (@PathVariable int idtipocliente) {
+		return this.tipoClienteService.findOne(idtipocliente);
+		
+		
+	}
+	
+	@PostMapping("")
+	public TipoCliente save(@RequestBody TipoCliente novoTipoCliente) {
+		return this.tipoClienteService.save(novoTipoCliente);
+	}
+	
+	@PatchMapping("/{idtipocliente}")
+	public TipoCliente update(@PathVariable Integer idtipocliente, @RequestBody TipoCliente alterarTipoCliente ) {
+		return this.tipoClienteService.update(idtipocliente, alterarTipoCliente);
+	}
+	
+	@DeleteMapping("/{idtipocliente}")
+	public void delete (@PathVariable Integer idtipocliente) {
+		this.tipoClienteService.delete(idtipocliente);
+	}
+	
 }
--- microservico/src/main/java/br/com/brq/services/TipoClienteService.java
+++ microservico/src/main/java/br/com/brq/services/TipoClienteService.java
@@ -1,6 +1,7 @@
 package br.com.brq.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,4 +18,53 @@ public class TipoClienteService {
 	public List<TipoCliente> findAll(){
 		return this.tipoClienteRepository.findAll();
 	}
+	
+	public TipoCliente findOne(Integer idtipocliente) {
+//		Optional<TipoCliente> opTipoCli = this.tipoClienteRepository.findById(idtipocliente); // pega o id se estiver presente
+//		
+//		if (opTipoCli.isPresent()) {
+//			return opTipoCli.get();
+//		}
+//		else {
+//			return null;
+//		}
+		return this.tipoClienteRepository.findById(idtipocliente).orElseThrow(() -> new RuntimeException ("Não encontrado"));
+	}
+	
+	
+	public TipoCliente save (TipoCliente novoTipoCliente) {
+		return this.tipoClienteRepository.save(novoTipoCliente);
+	}
+	
+	public TipoCliente update(Integer idtipocliente, TipoCliente alterarTipoCliente) {
+		Optional<TipoCliente> opTipoCli = this.tipoClienteRepository.findById(idtipocliente);//procura no banco
+		
+		if(opTipoCli.isPresent()) {
+			TipoCliente updated = opTipoCli.get();
+			updated.setDesctipocli(alterarTipoCliente.getDesctipocli());
+			return this.tipoClienteRepository.save(updated);
+			//se existir pega o usuario do banco e altera de acordo com a request
+			
+		}else {
+			
+			//return null;
+			throw new RuntimeException ("Registro não localizado");
+			
+		}
+		
+		
+	}
+	
+	public void delete (Integer idtipocliente) {
+		
+		try {
+			this.tipoClienteRepository.deleteById(idtipocliente);
+			
+		} catch (Exception e) {
+			throw new RuntimeException("Erro ao deletar registro" + e.getMessage());
+		}
+		
+	}
+	
+	
 }
