de32a85fcda5043563d53b066d8ec2a5834259ff
Guilherme Pessoa Correia <guilhermepc@brq.com>
2021-04-16 17:43:11 -0300

    primeira aula de Angular 4 types of Bindings

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent {//aqui fica o TYPESCRIPT
+  public title : string = 'frontend';
+
+  public nome : string;
+
+  public isDesabilitado : boolean = true;
+
+  public somar() : number{
+    return (1+1);
+  }
+
+  public onMeuBotaoClick(){
+    let i = 0;
+    alert('Mensagem...' + this.title)
+  }
+
+  public getTitle(){
+    return this.title;
+  }
+
+  public capturarDados(valor){
+    console.log(valor);
+    this.nome = valor;
+    // alert('aaaaa')
+  }
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrimeiroComponent } from './primeiro/primeiro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrimeiroComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
